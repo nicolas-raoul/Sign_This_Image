82c458cb5cb37e3f689eedba827a3ad5631a7fa1
Nicolas Raoul <nicolas.raoul@gmail.com>
2025-09-10 17:02:22 +0900

    v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.nicolasraoul.signthisimage"
         minSdk = 36
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
